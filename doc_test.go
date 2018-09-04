352676e91b1971204058ce224743daa53dbf5af0
Jiayu Yi <yi-jiayu@users.noreply.github.com>
2018-09-04 16:36:17 +0800

    Add example for NewWithCipher

--- doc_test.go
+++ doc_test.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/yi-jiayu/presents"
+	"golang.org/x/crypto/blowfish"
 )
 
 // This example show how to encode and decode IDs.
@@ -75,5 +76,32 @@ func Example_customAlphabet() {
 	fmt.Println(n)
 	// Output:
 	// tiKYxtU_2ZB
+	// 1213486160
+}
+
+// You can also provide your own cipher.Block implementation as long as it has a 64-bit block size.
+// This example shows how you can use the Blowfish cipher provided by the golang.org/x/crypto/blowfish package.
+func ExampleNewWithCipher() {
+	// 56-byte Blowfish key
+	key := make([]byte, 56)
+	c, err := blowfish.NewCipher(key)
+	if err != nil {
+		log.Fatal(err)
+	}
+	p, err := presents.NewWithCipher(c, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := p.Wrap(1213486160)
+	fmt.Println(s)
+
+	n, err := p.Unwrap(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(n)
+	// Output:
+	// woSdQdAYuiK
 	// 1213486160
 }
